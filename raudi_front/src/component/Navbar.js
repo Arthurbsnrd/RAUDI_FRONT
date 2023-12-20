ce0ace3051807f9497a365a253fd59e02438b9fe
Arthurbsnrd <arthurbesnard1@outlook.fr>
2023-12-20 17:35:24 +0100

    Nav

---
+++ raudi_front/src/component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Accueil</li>
+        <li>À propos</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+sss
