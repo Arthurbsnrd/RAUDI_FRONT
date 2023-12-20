609411a89d86a2706097169851802555f2106dd2
Arthurbsnrd <arthurbesnard1@outlook.fr>
2023-12-20 17:31:13 +0100

    APP

--- raudi_front/src/component/App.js
+++ raudi_front/src/component/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-
+import './App.css';
 
 function App() {
-  const user = null; // Replace this with actual authentication logic
-
-
-  coucou
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RAUDI Cars</h1>
+        <p>Discover the excellence in automotive engineering.</p>
+        <img
+          src="https://via.placeholder.com/400x200"
+          className="App-logo"
+          alt="RAUDI Logo"
+        />
+      </header>
+      <section className="CarModels">
+        <h2>Featured Models</h2>
+        <div className="CarList">
+          {/* Ajoutez les informations sur les modèles de voitures RAUDI */}
+          <div className="CarCard">
+            <img src="https://via.placeholder.com/200x100" alt="Car Model" />
+            <h3>RAUDI A1</h3>
+            <p>Compact, stylish, and powerful.</p>
+          </div>
+          {/* Ajoutez d'autres modèles de voitures ici */}
+        </div>
+      </section>
+      <footer className="App-footer">
+        <p>&copy; 2023 RAUDI Cars. All rights reserved.</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
